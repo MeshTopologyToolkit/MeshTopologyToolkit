4f60c4921a8158732e6f0c8978ebef90b397a397
Gleb <gleb@gleblebedev.com>
2025-12-07 11:36:57 +0000

    ChangeSpace now can transform vertex position in mesh. Doesn't change node positions yet.

--- src/MeshTopologyToolkit/Operators/ChangeSpace.cs
+++ src/MeshTopologyToolkit/Operators/ChangeSpace.cs
@@ -1,50 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 namespace MeshTopologyToolkit.Operators
 {
     public class ChangeSpace : ContentOperatorBase
     {
-        public ChangeSpace(Matrix4x4 transoform, bool flipV = false, bool flipFaceIndices = false)
+        Matrix4x4 _transform;
+        public ChangeSpace(Matrix4x4 transform, bool flipV = false, bool flipFaceIndices = false)
         {
-            if (transoform != Matrix4x4.Identity)
-                throw new NotImplementedException("ChangeSpace operator currently only supports identity transform.");
-            if (flipFaceIndices)
-                throw new NotImplementedException("ChangeSpace operator currently does not support flipping face indices.");
-
+            //if (transform != Matrix4x4.Identity)
+            //    throw new NotImplementedException("ChangeSpace operator currently only supports identity transform.");
+            //if (flipFaceIndices)
+            //    throw new NotImplementedException("ChangeSpace operator currently does not support flipping face indices.");
+            _transform = transform;
             FlipV = flipV;
+            FlipFaceIndices = flipFaceIndices;
         }
 
+        public Matrix4x4 Transoform => _transform;
+
         public bool FlipV { get; private set; }
+        public bool FlipFaceIndices { get; }
 
         public override IMesh Transform(IMesh mesh)
         {
-            if (!mesh.TryGetAttribute<Vector2>(MeshAttributeKey.TexCoord, out var texCoords))
-                return mesh;
-
             if (mesh is UnifiedIndexedMesh unifiedIndexedMesh)
             {
                 var res = new UnifiedIndexedMesh(unifiedIndexedMesh.Name);
                 foreach (var key in mesh.GetAttributeKeys())
                 {
-                    if (FlipV && key.Name == MeshAttributeNames.TexCoord)
+                    switch (key.Name)
                     {
-                        var data = new ListMeshVertexAttribute<Vector2>(texCoords.Count);
-                        foreach (var uv in texCoords)
-                        {
-                            var newUv = uv;
-                            newUv.Y = 1.0f - newUv.Y;
-                            data.Add(newUv);
-                        }
-                        res.SetAttribute(key, data);
-                    }
-                    else
-                    {
-                        res.SetAttribute(key, mesh.GetAttribute(key));
+                        case MeshAttributeNames.Position:
+                            {
+                                var positions = mesh.GetAttribute<Vector3>(key);
+                                var data = new ListMeshVertexAttribute<Vector3>(positions.Count);
+                                foreach (var position in positions)
+                                {
+                                    data.Add(Vector3.Transform(position, _transform));
+                                }
+                                res.SetAttribute(key, data);
+                            }
+                            break;
+                        case MeshAttributeNames.TexCoord:
+                            if (FlipV)
+                            {
+                                var texCoords = mesh.GetAttribute<Vector2>(key);
+                                var data = new ListMeshVertexAttribute<Vector2>(texCoords.Count);
+                                foreach (var uv in texCoords)
+                                {
+                                    var newUv = uv;
+                                    newUv.Y = 1.0f - newUv.Y;
+                                    data.Add(newUv);
+                                }
+                                res.SetAttribute(key, data);
+                            }
+                            break;
+                        default:
+                            res.SetAttribute(key, mesh.GetAttribute(key));
+                            break;
                     }
                 }
 
-                res.AddIndices(unifiedIndexedMesh.Indices);
+                res.AddIndices(TransformIndices((IReadOnlyList<int>)unifiedIndexedMesh.Indices, unifiedIndexedMesh.DrawCalls));
                 foreach (var drawCall in mesh.DrawCalls)
                 {
                     res.DrawCalls.Add(drawCall.Clone());
@@ -56,21 +75,37 @@ namespace MeshTopologyToolkit.Operators
                 var res = new SeparatedIndexedMesh(separatedIndexedMesh.Name);
                 foreach (var key in mesh.GetAttributeKeys())
                 {
-
-                    if (FlipV && key.Name == MeshAttributeNames.TexCoord)
+                    switch (key.Name)
                     {
-                        var data = new ListMeshVertexAttribute<Vector2>(texCoords.Count);
-                        foreach (var uv in texCoords)
-                        {
-                            var newUv = uv;
-                            newUv.Y = 1.0f - newUv.Y;
-                            data.Add(newUv);
-                        }
-                        res.SetAttribute(key, data, separatedIndexedMesh.GetAttributeIndices(key));
-                    }
-                    else
-                    {
-                        res.SetAttribute(key, separatedIndexedMesh.GetAttribute(key), separatedIndexedMesh.GetAttributeIndices(key));
+                        case MeshAttributeNames.Position:
+                            {
+                                var positions = separatedIndexedMesh.GetAttribute<Vector3>(key);
+                                var data = new ListMeshVertexAttribute<Vector3>(positions.Count);
+                                foreach (var uv in positions)
+                                {
+                                    data.Add(Vector3.Transform(uv, _transform));
+                                }
+                                res.SetAttribute(key, data, TransformIndices(separatedIndexedMesh.GetAttributeIndices(key), separatedIndexedMesh.DrawCalls));
+                            }
+                            break;
+                            case MeshAttributeNames.TexCoord:
+                            if (FlipV)
+                            {
+                                var texCoords = separatedIndexedMesh.GetAttribute<Vector2>(key);
+                                var data = new ListMeshVertexAttribute<Vector2>(texCoords.Count);
+                                foreach (var uv in texCoords)
+                                {
+                                    var newUv = uv;
+                                    newUv.Y = 1.0f - newUv.Y;
+                                    data.Add(newUv);
+                                }
+                                res.SetAttribute(key, data, TransformIndices(separatedIndexedMesh.GetAttributeIndices(key), separatedIndexedMesh.DrawCalls));
+                            }
+                            else
+                            {
+                                res.SetAttribute(key, separatedIndexedMesh.GetAttribute(key), TransformIndices(separatedIndexedMesh.GetAttributeIndices(key), separatedIndexedMesh.DrawCalls));
+                            }
+                            break;
                     }
                 }
 
@@ -83,6 +118,52 @@ namespace MeshTopologyToolkit.Operators
             }
 
             throw new NotSupportedException($"ChangeSpace operator does not support mesh type {mesh.GetType().FullName}");
+        }
+
+        private IReadOnlyList<int> TransformIndices(IReadOnlyList<int> originalIndices, IList<MeshDrawCall> drawCalls)
+        {
+            if (!FlipFaceIndices)
+                return originalIndices;
+
+            var indices = Copy(originalIndices);
+            foreach (var drawCall in drawCalls)
+            {
+                switch (drawCall.Type)
+                {
+                    case MeshTopology.Points:
+                    case MeshTopology.LineList:
+                    case MeshTopology.LineLoop:
+                    case MeshTopology.LineStrip:
+                        break;
+                    case MeshTopology.TriangleList:
+                        {
+                            for (int i = 0; i < drawCall.NumIndices; i += 3)
+                            {
+                                int idx0 = drawCall.StartIndex + i;
+                                int idx1 = drawCall.StartIndex + i + 1;
+                                int idx2 = drawCall.StartIndex + i + 2;
+                                // Swap idx1 and idx2 to flip the triangle
+                                int temp = indices[idx1];
+                                indices[idx1] = indices[idx2];
+                                indices[idx2] = temp;
+                            }
+                        }
+                        break;
+                    default:
+                        throw new NotImplementedException($"Flipping indices for {drawCall.Type} is not implemented.");
+                }
+            }
+            return indices;
+        }
+
+        private int[] Copy(IReadOnlyList<int> indices)
+        {
+            var copy = new int[indices.Count];
+            for (int i = 0; i < indices.Count; ++i)
+            {
+                copy[i] = indices[i];
+            }
+            return copy;
         }
     }
 }
